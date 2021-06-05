214f92515ce7b65c78b627b2218409d3353b02d1
rdankhara <rajnikant.dankhara@gmail.com>
2021-06-05 15:45:14 +0100

    added post construct hook

--- src/main/java/com/hary/service/SpeakerServiceImpl.java
+++ src/main/java/com/hary/service/SpeakerServiceImpl.java
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
-    @Autowired()
     public void setRepository(SpeakerRepository repository) {
         this.repository = repository;
     }
@@ -24,9 +24,15 @@ public class SpeakerServiceImpl implements SpeakerService {
         System.out.println("SpeakerServiceImpl no arg constructor");
     }
 
+    @Autowired()
     public SpeakerServiceImpl(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl repository constructor");
         this.repository = repository;
+    }
+
+    @PostConstruct
+    private void initialized() {
+        System.out.println("Invoked after construction");
     }
 
     @Override
